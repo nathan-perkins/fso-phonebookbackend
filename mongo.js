ae055a5cb534689a6e9215d50234a74b51705113
nathan-perkins <140360768+nathan-perkins@users.noreply.github.com>
2025-07-22 15:00:43 -0500

    complete exercise 3.12

---
+++ mongo.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://gperkinsn:${password}@phonebook.nqpvcf.mongodb.net/?retryWrites=true&w=majority&appName=phonebook`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv[3] && process.argv[4]) {
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4]
+    })
+
+    person.save().then(result => {
+        console.log(`added ${person.name} number ${person.number} to phonebook`)
+        mongoose.connection.close()
+    })
+} else {
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(person)
+        })
+        mongoose.connection.close()
+    })
+}
